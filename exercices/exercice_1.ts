cea485f5599b3e0cdc0bd46bf28ed1ca5a8c4dd8
Skuurt <djalil.arzour@gmail.com>
2023-03-08 14:03:16 +0100

    exo_typescript

---
+++ exercices/exercice_1.ts
@@ -0,0 +1,52 @@
+// Exercice # 1
+
+1. Récuperer le ticket du client.
+2. Regarder si:
+1. Le chiffre sur le ticket est compris entre 1 & 5.
+
+                Si oui:
+
+Dire: "Vous avez gagné bravo !"
+
+                Si non:
+
+DIre: "Tentez votre chance à nouveau ! "
+
+// Exerice # 2
+
+1. Compter le nombre d'articles que le client possède.
+
+2. Lister chaque produit que possède le client.
+
+3. Additionner le prix de chaque item de la liste.
+
+4. Multiplier le résultat par 1, 196.
+
+
+// Exercice # 3
+
+// - Typer les constantes suivantes :
+const organism: String = "Simplon" ==> 
+const learners: Number = 15 ==> Number
+const promotionStarted: boolean = true
+const learnerNames any[] = ["Vincent", "Toufik", "Tom", "Samuel", "Orhan"]
+const pedagogicalReferent Record<string, string > = { name: "Tiphaine", work: "Simplon" }
+const absentLearner: undefined = undefined
+
+
+// Exercice # 4
+
+function exercice4() {
+    const country: string = "France"
+    console.log(country)
+}
+
+// Exercice # 5
+
+function exercice5() {
+    const firstName: string = 'Djalil'
+    const lastName: string = 'Arzour'
+
+    console.log(firstName + ' ' + lastName)
+    console.log(`$(firstName) $(lastName`)
+}
