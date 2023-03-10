86c4d6710d20c9aa337155434b0fed4585a3945a
Skuurt <djalil.arzour@gmail.com>
2023-03-10 11:57:29 +0100

    Exo_TS

--- demo.ts
+++ demo.ts
@@ -1,19 +1,32 @@
-// Système de constante et variable
-// Différent types
+// Exercice 1.3
 
-const firstName: string = 'Kévin';
-const lastName: string = 'Wolff'; 
+const Names: string[] = ['Théo', 'Martin', 'Lucas', 'Antoine', 'Etienne']
 
-const age: number = 30
-
-const isHeWakeUp: boolean = false;
-
-// Tableau
-const learners: any[] = ['Kevin', 30, true]
-
-// Objet
-const learner: any = {
-    name: 'Kevin',
-    age: 30,
-    wakeUp: false
+for (let i = 0; i < Names.length; i++) {
+    if (Names[i].length <= 5)
+        console.log(Names[i]);
 }
+
+// Exercice 2.3
+
+const Countries: string[] = ['France', 'Japon', 'Maroc', 'Irlande', 'Italie']
+const sentence: string = ''
+for (let i = 0; i < Countries.length; i++)
+    console.log(Countries + sentence)
+
+
+// Exercice 3.3
+
+const Numbers: number[] = [123, 999, 340, 12390]
+const Numbersmultiplied = Numbers.map(num => num * 180);
+
+console.log(Numbersmultiplied);
+
+const numbers: number[] = [123, 999, 340, 12390];
+let results: number[] = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    results.push(numbers[i] * 180);
+}
+
+console.log(results);
